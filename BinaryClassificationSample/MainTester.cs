f4c75b5be4704e52ef95f642ad7b4ced9d00ce80
Lorenzo Maiorfi <maiorfi@gmail.com>
2019-10-27 09:57:26 +0100

    Casi d'uso principali

--- BinaryClassificationSample/MainTester.cs
+++ BinaryClassificationSample/MainTester.cs
@@ -1,14 +1,85 @@
 using System;
+using BinaryClassifier;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace BinaryClassificationSample
 {
-    public class UnitTest1
+    public class MainTester
     {
+        private readonly ITestOutputHelper _output;
+
+        public MainTester(ITestOutputHelper output)
+        {
+            this._output = output;
+        }
+        
         [Fact]
         public void Test_Tester_is_Working_As_Expected()
         {
+            _output.WriteLine("Test_Tester_is_Working_As_Expected()...");
             Assert.True(true);
+            _output.WriteLine("...Test_Tester_is_Working_As_Expected DONE.");
+        }
+
+        [Fact]
+        public void Test_Evaluate_Prediction_Model_Score()
+        {
+            _output.WriteLine("Test_Evaluate_Prediction_Model_Score()...");
+
+            var predictor = new Predictor();
+            
+            predictor.LoadTestData();
+            predictor.BuildAndTrainModel();
+            var metrics = predictor.EvaluateModelMetrics();
+            
+            _output.WriteLine($"Model Metrics - Accuracy:{metrics.Accuracy:P2}, AreaUnderRocCurve:{metrics.AreaUnderRocCurve:P2}, F1Score:{metrics.F1Score:P2}");
+            
+            Assert.InRange(metrics.Accuracy,0.5, 0.95);
+            
+            _output.WriteLine("...Test_Evaluate_Prediction_Model_Score() DONE.");
+        }
+        
+        [Fact]
+        public void Test_Predict_Output_Positive()
+        {
+            _output.WriteLine("Test_Predict_Output_Positive()...");
+
+            const string INPUT_DATA = "Food was very good";
+            
+            var predictor = new Predictor();
+            
+            predictor.LoadTestData();
+            predictor.BuildAndTrainModel();
+
+            var predictionTuple = predictor.Predict(INPUT_DATA);
+            
+            _output.WriteLine($"Prediction for \"{INPUT_DATA}\" : {predictionTuple.prediction} (Probability: {predictionTuple.probability}, Score: {predictionTuple.score})");
+            
+            Assert.True(predictionTuple.prediction);
+            
+            _output.WriteLine("...Test_Predict_Output_Positive() DONE.");
+        }
+        
+        [Fact]
+        public void Test_Predict_Output_Negative()
+        {
+            _output.WriteLine("Test_Predict_Output_Negative()...");
+
+            const string INPUT_DATA = "This was quite a horrible meal";
+            
+            var predictor = new Predictor();
+            
+            predictor.LoadTestData();
+            predictor.BuildAndTrainModel();
+
+            var predictionTuple = predictor.Predict(INPUT_DATA);
+            
+            _output.WriteLine($"Prediction for \"{INPUT_DATA}\" : {predictionTuple.prediction} (Probability: {predictionTuple.probability}, Score: {predictionTuple.score})");
+            
+            Assert.False(predictionTuple.prediction);
+            
+            _output.WriteLine("...Test_Predict_Output_Negative() DONE.");
         }
     }
 }
--- BinaryClassifier/Predictor.cs
+++ BinaryClassifier/Predictor.cs
@@ -1,9 +1,61 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using Microsoft.ML;
+using Microsoft.ML.Data;
 
 namespace BinaryClassifier
 {
     public class Predictor
     {
         private const string DATA_PATH = @"C:\Progetti\Lab\NetCore\ML\BinaryClassificationSample\data\yelp_labelled.txt";
+
+        MLContext _mlContext = new MLContext();
+
+        private IDataView _allData;
+        
+        private IDataView _trainData;
+        private IDataView _testData;
+
+        private ITransformer _model;
+        
+
+        public void LoadTestData()
+        {
+            _allData = _mlContext.Data.LoadFromTextFile<SentimentData>(DATA_PATH, hasHeader: false);
+
+            var trainingSplitData = _mlContext.Data.TrainTestSplit(_allData, testFraction: 0.2);
+            
+            _trainData = trainingSplitData.TrainSet;
+            _testData = trainingSplitData.TestSet;
+        }
+
+        public void BuildAndTrainModel()
+        {
+            var estimator = _mlContext.Transforms.Text.FeaturizeText(outputColumnName: "Features", inputColumnName: nameof(SentimentData.SentimentText))
+                .Append(_mlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: "Features"));
+
+            _model = estimator.Fit(_testData);
+        }
+
+        public CalibratedBinaryClassificationMetrics EvaluateModelMetrics()
+        {
+            var predictions = _model.Transform(_trainData);
+
+            return _mlContext.BinaryClassification.Evaluate(predictions, "Label");
+        }
+
+        public (bool prediction, float score, float probability) Predict(string inputData)
+        {
+            var predictionFunction = _mlContext.Model.CreatePredictionEngine<SentimentData, SentimentPrediction>(_model);
+
+            var inputSample = new SentimentData()
+            {
+                SentimentText = inputData
+            };
+
+            var resultPrediction = predictionFunction.Predict(inputSample);
+
+            return (resultPrediction.Prediction, resultPrediction.Score, resultPrediction.Probability);
+        }
     }
 }
