c02d32a486cb994f9440fc93dffc952d01073960
Lorenzo Maiorfi <maiorfi@innovactive.it>
2019-10-27 09:56:35 +0100

    Initial commit

---
+++ BinaryClassificationSample/MainTester.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace BinaryClassificationSample
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test_Tester_is_Working_As_Expected()
+        {
+            Assert.True(true);
+        }
+    }
+}
---
+++ BinaryClassifier/Predictor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace BinaryClassifier
+{
+    public class Predictor
+    {
+        private const string DATA_PATH = @"C:\Progetti\Lab\NetCore\ML\BinaryClassificationSample\data\yelp_labelled.txt";
+    }
+}
